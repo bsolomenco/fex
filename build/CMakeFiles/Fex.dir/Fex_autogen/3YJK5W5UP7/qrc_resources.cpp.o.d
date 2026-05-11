3c92886c2233942ed6a4a3a6cfa77449fe1a2e55
Rares <rares.petre897@gmail.com>
2026-05-11 08:40:08 +0300

    [FIX]gitinore

--- build/CMakeFiles/Fex.dir/Fex_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Fex.dir/Fex_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/petrera@pms.brasov/Documents/c++/fex/build/Fex_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Fex.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Fex.
-# This may be replaced when dependencies are built.
