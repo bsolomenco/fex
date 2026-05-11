a09f971e200fb238f84deea92b28bf951fd49969
Rares <rares.petre897@gmail.com>
2026-05-11 11:42:41 +0300

    clean code MainWindow.cpp

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -5,13 +5,9 @@ MainWindow::MainWindow(QWidget* parent)
 {    
     QWidget* centralWidget = new QWidget(this);
     setCentralWidget(centralWidget);
-
     _label = new QLabel("Hello World!");
-    _layout = new QVBoxLayout(centralWidget);
-    
+    _layout = new QVBoxLayout(centralWidget);    
     _label->setAlignment(Qt::AlignCenter);
-    setFixedSize(650, 400);
-    
+    setFixedSize(650, 400);    
     _layout->addWidget(_label);
-
 }
